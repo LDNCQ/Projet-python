5afb05f01c952d18f03be9649c2f97d263847883
LDNCQ <willylin11@gmail.com>
2023-12-01 10:33:23 +0100

    Optimisation des potions

--- items.py
+++ items.py
@@ -7,13 +7,11 @@ class Items:
 class HealingPotion(Items):
     def __init__(self):
         self.name = "Potion de soin"
-        self.stat_type = "hp"
+
         self.stat_boost = 10
 
     def use(self, player):
-        value = getattr(player, self.stat_type)
-        new_value = value + self.stat_boost
-        setattr(player, self.stat_type, new_value)
+        player.hp += self.stat_boost
         if player.hp > player.max_hp:
             player.hp = player.max_hp
         print(f"{player.name} a utilisé une {self.name} et a récupéré {self.stat_boost} points de vie.")
@@ -21,31 +19,23 @@ class HealingPotion(Items):
 class AttackPotion(Items):
     def __init__(self):
         self.name = "Potion d'attaque"
-        self.stat_type = "atk"
         self.stat_boost = 5
 
     def use(self, player):
-        value = getattr(player, self.stat_type)
-        new_value = value + self.stat_boost
-        setattr(player, self.stat_type, new_value)
+        player.atk += self.stat_boost
         print(f"{player.name} a utilisé une {self.name} et a gagné {self.stat_boost} points de d'attaque.")
 
 
 class DefensePotion(Items):
     def __init__(self):
         self.name = "Potion de défense"
-        self.stat_type = "defense"
         self.stat_boost = 4
 
     def use(self, player):
-        value = getattr(player, self.stat_type)
-        new_value = value + self.stat_boost
-        setattr(player, self.stat_type, new_value)
+        player.defense += self.stat_boost
         print(f"{player.name} a utilisé une {self.name} et a gagné {self.stat_boost} points de défense.")
 
 
 healpotion = HealingPotion()
-
 atkpotion = AttackPotion()
-
 defpotion = DefensePotion()
